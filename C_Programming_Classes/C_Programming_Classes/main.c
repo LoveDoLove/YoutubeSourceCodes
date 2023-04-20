e033ca56d43dd173e7b273ec0a07fbc7bc8890b6
LoveDoLove <v0130p5100cuboss@gmail.com>
2023-04-20 20:49:31 +0800

    update

--- C_Programming_Classes/C_Programming_Classes/main.c
+++ C_Programming_Classes/C_Programming_Classes/main.c
@@ -2,6 +2,64 @@
 #include<stdlib.h>
 #pragma warning(disable:4996)
 
-void main() {
+struct Employee {
+	char name[21];
+	int age;
+	double ranking;
+};
 
+void input();
+void chkRanking();
+void displayRanking(double ranking);
+
+void main() {
+	input();
+	chkRanking();
+}
+
+void input() {
+	struct Employee emp;
+	FILE* file = fopen("employee.txt", "w");
+	if (!file) {
+		printf("Open file error...\n");
+		exit(-1);
+	}
+	printf("Enter employee name (XXX to stop): ");
+	rewind(stdin);
+	gets(emp.name);
+	while (strcmp(emp.name, "XXX") != 0) {
+		printf("Enter age: ");
+		scanf("%d", &emp.age);
+
+		printf("Enter ranking: ");
+		scanf("%lf", &emp.ranking);
+
+		printf("\n");
+		fprintf(file, "%s|%d|%f\n", emp.name, emp.age, emp.ranking);
+
+		printf("Enter employee name (XXX to stop): ");
+		rewind(stdin);
+		gets(emp.name);
+	}
+	printf("\n\n");
+	fclose(file);
+}
+
+void chkRanking() {
+	struct Employee emp;
+	FILE* file = fopen("employee.txt", "r");
+	printf("Listing of Employess (Ranking above 3)\n");
+	printf("--------------------------------------\n");
+	while (fscanf(file, "%[^|]|%d|%lf\n", emp.name, &emp.age, &emp.ranking) != EOF) {
+		if (emp.ranking > 3.0) {
+			printf("Name: %s\n", emp.name);
+			printf("Age: %d\n", emp.age);
+			displayRanking(emp.ranking);
+			printf("\n\n");
+		}
+	}
+}
+
+void displayRanking(double ranking) {
+	printf("Ranking: %.2f\n", ranking);
 }
